cadb2c6e8aa4071dd34cf171f74cb194084d8532
sarathlalup <56441804+sarathlalup@users.noreply.github.com>
2019-11-12 03:44:58 -0800

    Update and rename r.cmd to tools.cmd

---
+++ Forensics/File Forensics/tools.cmd
@@ -0,0 +1,19 @@
+SlackPirate - https://github.com/emtunc/SlackPirate/tree/0c15149212bf16fcb3545848fdb790b2262fb69c
+autopsy - https://github.com/sleuthkit/autopsy/tree/31795cec1197f298b9b5d194c5c74ca1c8f83a29
+dff - https://github.com/arxsys/dff/tree/d40d46b0501fe7a8d742538842f2c2a4333dd8e0
+docker-explorer - https://github.com/google/docker-explorer/tree/7646e8a0b00794733dc14d1976664309fd9fa72f
+hadoop_framework - https://github.com/sleuthkit/hadoop_framework/tree/a855c40733ff9ba1d0fd2345a1fd6cbca668f0c6
+osxcollector - https://github.com/Yelp/osxcollector/tree/1e98f4650cdcb027ae350518040561b59b33fed1
+scalpel - https://github.com/sleuthkit/scalpel/tree/47815c21172978b423be496e8451604ccd124137
+shellbags - https://github.com/williballenthin/shellbags/tree/fee76eb25c2b80c33caf8ab9013de5cba113dcd2
+sleuthkit - https://github.com/sleuthkit/sleuthkit/tree/c2b93677c4bd56812e40030fefba69a0bc9c735d
+telegram-extractor - https://github.com/tsusanka/telegram-extractor/tree/b7ff242963efd376c8165318d04073046936bf9a
+truehunter - https://github.com/adoreste/truehunter/tree/0a2895d90de17910ab2eab5addd1d0c01fc6959a
+
+
+
+
+
+
+
+
